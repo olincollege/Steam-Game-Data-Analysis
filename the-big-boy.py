f373840f0fca36bc35460bbcf133b73fafa07921
Lily <lilycwei2027@gmail.com>
2024-03-12 13:53:54 -0400

    committing files for getting review data

--- the-big-boy.py
+++ the-big-boy.py
@@ -1,6 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get("https://store.steampowered.com/charts/mostplayed").content
+soup = BeautifulSoup(open("steam-game-HTMLs/Grand_Theft_Auto_V.html"), "html.parser")
+temp_text = open("temp_text", "w")
+temp_text.write(soup.get_text())
+temp_text.close()
+
+myspan = soup.find_all("span", {"class": "nonresponsive_hidden responsive_reviewdesc"})
+print(myspan)
+
+"""r = requests.get("https://store.steampowered.com/charts/mostplayed").content
 test_text = open("steam-game-HTMLs/mostplayed", "wb")
 test_text.write(r)
-test_text.close()
+test_text.close()"""
