aded70b81bcbffb5dc950b14534ed554793d68d7
lilyWay27 <lilycwei2027@gmail.com>
2024-03-15 11:14:28 -0400

    edit to not skip over index

--- write_data.py
+++ write_data.py
@@ -31,7 +31,9 @@ dataframe_titles = {
 df = pd.DataFrame(dataframe_titles)
 
 count = 0
+index = 0
 for link in links:
+    print(count, index)
     if prices[count] != -1:
         html = requests.get(link).content
         soup = BeautifulSoup(html, "html.parser")
@@ -39,7 +41,7 @@ for link in links:
         name = get_data.get_name(link)
         percent, num = get_data.get_reviews(soup)
         genre = get_data.get_game_genre(soup)
-        df.loc[links.index(link)] = {
+        df.loc[index] = {
             "Game Name": name,
             "Percent Positive Reviews": percent,
             "Number of Reviews": num,
@@ -49,8 +51,7 @@ for link in links:
             "Price": prices[count],
             "Peak Number of Players": peak_players[count],
         }
-    
-    print(count)
+        index += 1
     count += 1
     time.sleep(5)
 df.to_csv("steam_data.csv", sep="\t")
