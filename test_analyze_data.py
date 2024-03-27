5d50e31bbc32c5b01e215b1846f888493761155b
fiona-giraffe <fi.mcsherry@gmail.com>
2024-03-27 11:34:43 -0400

    Write unit tests

--- test_analyze_data.py
+++ test_analyze_data.py
@@ -3,30 +3,32 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 
 from get_data import (
-    get_tbody,
-    get_game_links,
     get_price_and_peak,
     get_name,
     get_reviews,
     get_game_genre,
 )
 
-soup_test_data_list = [
-    bs("<div></div>", "html.parser"),
-    bs("<tbody>test</tbody>", "html.parser"),
-]
+df = pd.read_csv("steam_data.csv")
 
 # Define sets of test cases.
-get_tbody_cases = [
-    # Check html with no tbody
-    (soup_test_data_list[0], ""),
-    # Check html containing only tbody
-    (soup_test_data_list[1], "test"),
+
+get_price_and_peak_cases = [
+    # Check that a game that is free has a price of 0
+    (df.loc[0].at["Price"], 0.0),
+    # Check a game with a price under 10
+    (df.loc[17].at["Price"], 3.99),
+    # Check a game with a price under 20
+    (df.loc[4].at["Price"], 14.99),
+    # Check a game with a price under 30
+    (df.loc[27].at["Price"], 29.99),
+    # Check a game with a price under 40
+    (df.loc[10].at["Price"], 39.99),
+    # Check a game with a price under 50
+    (df.loc[22].at["Price"], 44.99),
+    # Check a game with a price above 50
+    (df.loc[36].at["Price"], 59.99),
 ]
-
-get_game_links_cases = []
-
-get_price_and_peak_cases = []
 
 get_name_cases = []
 
@@ -37,19 +39,21 @@ get_game_genre_cases = []
 
 # Define standard testing functions to check functions' outputs given certain
 # inputs defined above.
-@pytest.mark.parametrize("soup,tbody", get_tbody)
-def test_get_tbody(soup, tbody):
+@pytest.mark.parametrize("price", get_price_and_peak_cases)
+def test_get_tbody(price, peak, price_exp, peak_exp):
     """
-    Test that retrieving the body text from a beautiful soup object
-    returns the correct result of what is between <tbody> and </tbody>
+    Test that the price and peak data in the csv file (acquired from
+    running get_price_and_peak) is the expected result.
 
     Args:
-        soup: A beautiful soup object of test html data
-        tbody: A string of the expected results of the function
+        price: The price in the csv file
+        peak: The peak number of players in the csv file
+        price_exp: A string representing the expected price
+        peak_exp: A string representing the expected peak players
     """
-    result = get_tbody(soup)
-    assert isinstance(result, str)
-    assert result == tbody
+    assert isinstance(price, str, peak, str)
+    assert price_exp == price
+    assert peak_exp == peak
 
 
 @pytest.mark.parametrize("tbody,links", get_game_links)
